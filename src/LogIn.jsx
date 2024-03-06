919e2676b1b85eeb8ae66b350cfd0c4b13ac6103
GradinaruAlexandra <147167654+GradinaruAlexandra@users.noreply.github.com>
2024-03-06 17:48:36 +0200

    first commit

---
+++ src/LogIn.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LOGO from "./assets/bg.png";
+import TITLE from "./assets/titlul.png";
+
+function Login() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="background">
+        
+      </div>
+    </>
+  );
+}
+
+export default Login;
