ce83802c69f9aed68f55eb3aa067af57d6f5c30d
unknown <tcaciucraluca2000@gmail.com>
2024-03-12 16:59:06 +0200

    mainmenuv1

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -1,16 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom"; //
 import "../App.css";
 
-
-function Mainmenu() {
-
-
+function MainMenu() {
   return (
     <>
       <div className="background">
-        
+        <div>
+          <img src="./images/title.png" className="titleimage" alt="" />
+          <img src="./video/crown.gif" className="crown" />
+          <div className="buttonsbox">
+            <div className="form">
+              <button className="button">
+                <span>Singleplayer</span>
+                <img src="public\video\player.gif" className="playergif" />
+              </button>
+              <button className="button">
+                <span>Multiplayer</span>
+                <img
+                  src="public\video\player.gif"
+                  className="multiplayergif1"
+                />
+                <img
+                  src="public\video\player.gif"
+                  className="multiplayergif2"
+                />
+                <img
+                  src="public\video\player.gif"
+                  className="multiplayergif3"
+                />
+              </button>
+              <button className="button">
+                <span>Settings</span>
+                <img src="public\video\settings.gif" className="settingsgif" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default Mainmenu;
+export default MainMenu;
