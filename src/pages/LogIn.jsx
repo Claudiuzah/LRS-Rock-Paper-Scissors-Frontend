d841cfc5d6dc49c44be3f069e6d5529abc40dee8
GradinaruAlexandra <147167654+GradinaruAlexandra@users.noreply.github.com>
2024-03-12 15:34:35 +0200

    log in form

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,13 +1,41 @@
 import "../App.css";
+// import Loginbox from "./components/loginbox";
 
 function Login() {
   return (
     <>
       <div className="background">
+        <div className="containertitle">
+          <img src="./images/title.png" className="titleimage" alt="" />
+          <img src="./video/crown.gif" className="crown" alt="" />
+        </div>
+
+        {/* <Loginbox/> */}
+
         <div>
-          <img src="./images/titlul.png" className="titleimage" alt="" />
-          <img src="./video/crown.gif" className="crown" />
-          {/* <button>Log in</button> */}
+          <div className="loginbox">
+            <div className="content">
+              <div className="contenth2">
+                <h2>Sign In</h2>
+              </div>
+
+              <div className="form">
+                <div className="inputbox">
+                  <input type="text" required className="input" />
+                  <i className="i">username</i>
+                </div>
+
+                <div class="inputbox">
+                  <input type="password" required className="input" />
+                  <i className="i">password</i>
+                </div>
+
+                <div class="inputbox">
+                  <input type="submit" value="Login" className="buttonlogin" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
---
+++ src/pages/components/loginbox.jsx
@@ -0,0 +1,35 @@
+// import ".\src\App.css";
+
+// function Loginbox() {
+//   return (
+//     <>
+//       <div>
+//           <div className="loginbox">
+//             <div className="content">
+//               <div className="contenth2">
+//                 <h2>Sing In</h2>
+//               </div>
+
+//               <div className="form">
+//                 <div className="inputbox">
+//                   <input type="text" required className="input" />
+//                   <i className="i">username</i>
+//                 </div>
+
+//                 <div class="inputbox">
+//                   <input type="password" required className="input" />
+//                   <i className="i">password</i>
+//                 </div>
+
+//                 <div class="inputbox">
+//                   <input type="submit" value="Login" className="buttonlogin" />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//     </>
+//   );
+// }
+
+// export default Loginbox;
