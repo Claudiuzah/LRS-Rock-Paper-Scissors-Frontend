92d87d48883b1deac957e91212f266f394b0456a
unknown <tcaciucraluca2000@gmail.com>
2024-03-19 09:08:13 +0200

    new

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -6,37 +6,47 @@ function MainMenu() {
   return (
     <>
       <div className="background">
-        <div>
-          <img src="./images/title.png" className="titleimagemenu" alt="" />
-          <img src="./video/crown.gif" className="crownmenu" />
-          <div className="buttonsboxmenu">
-            <div className="formmenu">
-              <button className="buttonmenu">
-                <span>Singleplayer</span>
-                <img src="public\video\player.gif" className="playergifmenu" />
-              </button>
-              <button className="buttonmenu">
-                <span>Multiplayer</span>
-                <img
-                  src="public\video\player.gif"
-                  className="multiplayergif1menu"
-                />
-                <img
-                  src="public\video\player.gif"
-                  className="multiplayergif2menu"
-                />
-                <img
-                  src="public\video\player.gif"
-                  className="multiplayergif3menu"
-                />
-              </button>
-              <button className="buttonmenu">
-                <span>Settings</span>
-                <img
-                  src="public\video\settings.gif"
-                  className="settingsgifmenu"
-                />
-              </button>
+        <img src="./images/title.png" className="titleimagemenu" />
+        <img src="./video/crown.gif" className="crownmenu" />
+
+        <div class="crownmenu"></div>
+      </div>
+
+      <div className="buttonsboxmenu">
+        <div className="formmenu">
+          <button className="buttonmenu">
+            <span>Singleplayer</span>
+            <img src="public\video\player.gif" className="playergifmenu" />
+          </button>
+          <button className="buttonmenu">
+            <span>Multiplayer</span>
+            <img
+              src="public\video\player.gif"
+              className="multiplayergif1menu"
+            />
+            <img
+              src="public\video\player.gif"
+              className="multiplayergif2menu"
+            />
+            <img
+              src="public\video\player.gif"
+              className="multiplayergif3menu"
+            />
+          </button>
+          <button className="buttonmenu">
+            <span>Settings</span>
+            <img src="public\video\settings.gif" className="settingsgifmenu" />
+          </button>
+          <div className="my-component">
+            <div className="player-avatar">
+              <img src="public\images\avatar.png" alt="Player Avatar" />
+            </div>
+            <div className="player-info">
+              <div className="player-name">Player Name</div>
+              <div className="player-stats">
+                Total wins: 0<br />
+                Score: 0
+              </div>
             </div>
           </div>
         </div>
@@ -46,3 +56,18 @@ function MainMenu() {
 }
 
 export default MainMenu;
+
+{
+  /* <div className="my-component">
+  <div className="player-avatar">
+    <img src="avatar.jpg" alt="Player Avatar" />
+  </div>
+  <div className="player-info">
+    <div className="player-name">Player Name</div>
+    <div className="player-stats">
+      Total wins: 0<br />
+      Score: 0
+    </div>
+  </div>
+</div>; */
+}
