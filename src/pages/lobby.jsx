9b7f1524b17bf05e7d6701cb99822dcb2cc01c2f
unknown <tcaciucraluca2000@gmail.com>
2024-03-21 16:33:16 +0200

    nou

---
+++ src/pages/lobby.jsx
@@ -0,0 +1,7 @@
+import "../App.css";
+
+function Multiplayer() {
+  return <div className="background"></div>;
+}
+
+export default Multiplayer;
--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import Startpage from "./pages/StartPage";
 import Login from "./pages/LogIn";
 import Mainmenu from "./pages/MainMenu";
 import Register from "./pages/SignUp";
-
+import Multiplayer from "./pages/lobby";
 
 const router = createBrowserRouter([
   {
@@ -22,14 +22,16 @@ const router = createBrowserRouter([
     path: "/menu",
     element: <Mainmenu />,
   },
-
+  {
+    path: "/lobby",
+    element: <Multiplayer />,
+  },
 ]);
 
 function Router() {
-
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
   );
 }
