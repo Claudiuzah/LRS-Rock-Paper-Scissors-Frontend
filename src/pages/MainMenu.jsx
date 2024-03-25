2c43b4ea7a7dec7c59b0f7a733f3924e8e23159e
unknown <tcaciucraluca2000@gmail.com>
2024-03-25 17:04:35 +0200

    fara

--- src/pages/MainMenu.jsx
+++ src/pages/MainMenu.jsx
@@ -7,39 +7,27 @@ function MainMenu() {
     <>
       <div className="background">
         <div className="groupcontainer_menu">
-          <div>
-            <img src="./images/title.png" className="titleimagemenu" alt="" />
-            <img src="./video/crown.gif" className="crownmenu" />
-            <div class="crownmenu"></div>
-          </div>
+          <img src="./images/title.png" className="titleimagemenu" alt="" />
+          <img src="./video/crown.gif" className="crownmenu" />
 
           <div className="buttonsboxmenu">
             <div className="formmenu">
               <button className="buttonmenu">
                 <span>Singleplayer</span>
-                <img src="public\video\player.gif" className="playergifmenu" />
+                <img src="video/player.gif" className="playergifmenu" />
               </button>
               <Link to="/lobby">
                 <button className="buttonmenu">
                   <span>Multiplayer</span>
-                  <img
-                    src="public\video\player.gif"
-                    className="multiplayergif1menu"
-                  />
-                  <img
-                    src="public\video\player.gif"
-                    className="multiplayergif2menu"
-                  />
-                  <img
-                    src="public\video\player.gif"
-                    className="multiplayergif3menu"
-                  />
+                  <img src="video/player.gif" className="multiplayergif1menu" />
+                  <img src="video/player.gif" className="multiplayergif2menu" />
+                  <img src="video/player.gif" className="multiplayergif3menu" />
                 </button>
               </Link>
               <button className="buttonmenu">
                 <span>Settings</span>
                 <img
-                  src="public\video\settings.gif"
+                  src="public/video/settings.gif"
                   className="settingsgifmenu"
                 />
               </button>
