acca791f34e49e95eb1fed7b39d4288d957dd68f
FloceaCosmin <flocea_cosmin@yahoo.com>
2024-04-16 17:33:23 +0300

    MUlt

--- src/pages/MultiPlayer.jsx
+++ src/pages/MultiPlayer.jsx
@@ -1,19 +1,46 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function MultiPlayer() {
+  const [remainingTime, setRemainingTime] = useState(15);
+
+  useEffect(() => {
+    let timer;
+
+    const updateTimer = () => {
+      setRemainingTime((prevTime) => prevTime - 1);
+    };
+
+    timer = setInterval(updateTimer, 1000);
+
+    return () => {
+      clearInterval(timer);
+      setRemainingTime(20);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (remainingTime === 0) {
+      setRemainingTime(20);
+    }
+  }, [remainingTime]);
+
   return (
     <div>
-      <div className='left'>
-        <Link to='/menu'>
-          <button className='exitbutton'>
-            <img src='video/exist.gif' className='exitgif' />
-          </button>
-        </Link>
+      <Link to='/menu'>
+        <button className='exitbutton'>
+          <img src='video/exist.gif' className='exitgif' alt='Exit' />
+        </button>
+      </Link>
+      <img src='video/clock.gif' className='clockM' alt='Clock' />
+      <div className='clockContainer'>
+        <div id='timer' className='timer'>
+          {remainingTime}
+        </div>
       </div>
-      <div>
-        <img src='video/clock.gif' className='clockM' />
-        <img src='images/table.png' className='tableM' />
+      <div className='container'>
+        <img src='images/table.png' className='tableM' alt='Table' />
       </div>
     </div>
   );
