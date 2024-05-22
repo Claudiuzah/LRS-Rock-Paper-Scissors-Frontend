886f376f8fd9ba4b6eaaca2b3099fe2f0b7baecf
Claudiuzah <147402261+Claudiuzah@users.noreply.github.com>
2024-05-22 07:33:33 +0300

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,5 @@
-export const API_SELF = 'http://172.16.1.42:8000';
+//export const API_SELF = 'http://172.16.1.42:8000';
+export const API_SELF = 'https://lrsback-lrs-bd4d9a06.koyeb.app';
 export const AUTHENTICATE = '/auth/register';
 export const LOGIN = '/auth/token';
 export const LOGOUT = '/auth/logout';
