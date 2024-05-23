924fecfe6b9efad8ecf639af38ebbc9be1e1968f
FloceaCosmin <flocea_cosmin@yahoo.com>
2024-05-23 16:49:30 +0300

    nou3

--- src/pages/leaderboard/index.jsx
+++ src/pages/leaderboard/index.jsx
@@ -1,34 +1,18 @@
 import styles from './index.module.css';
-import { Link } from 'react-router-dom';
-// import { useDisclosure } from '@mantine/hooks';
-// import { Modal } from '@mantine/core';
-// import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
-// import { useEffect } from 'react';
-// import useSignOut from 'react-auth-kit/hooks/useSignOut';
+import useSignOut from 'react-auth-kit/hooks/useSignOut';
 
 function LeaderBoard() {
-  //   const [opened, { open, close }] = useDisclosure(false);
-  //   const [currentVolume, setCurrentVolume] = useState();
-  //   const [currentSoundType, setCurrentSoundType] = useState();
-  //   const navigate = useNavigate();
+  const navigate = useNavigate();
   const auth = useAuthUser();
   console.log(auth);
 
-  //   const signOut = useSignOut();
+  const signOut = useSignOut();
 
-  //   useEffect(() => {
-  //     if (!auth) {
-  //       navigate('/auth');
-  //       console.log('User is not logged in.');
-  //     } else {
-  //       console.log('User is logged in.');
-  //     }
-  //   }, [auth]);
-  //   if (!auth) return;
   return (
     <main>
-      {/* <button
+      <button
         type='submit'
         onClick={() => {
           signOut();
@@ -36,33 +20,48 @@ function LeaderBoard() {
         }}
       >
         Sign Out
-      </button> */}
+      </button>
       <div>
         <div className={styles.groupContainerMenu}>
+          GOOD WORK
+          {/* <img src='./images/title.png' className={styles.titleImageMenu} /> */}
           <div className={styles.buttonsBoxMenu}>
             <div className={styles.formMenu}>
-              <Link to='/menu'>
-                <button className={styles.buttonMenu}>
-                  <span>Main Menu</span>
-                  <img src='video/player.gif' className={styles.playerGifMenu} />
-                </button>
-              </Link>
-              <Link to='/lobby'>
-                <button className={styles.buttonMenu}>
-                  <span>Lobby</span>
+              <div className={styles.buttonContainer}>
+                <Link to='/single' className={styles.buttonMenu}>
+                  Main Menu
+                  <div>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      x='0px'
+                      y='0px'
+                      width='50'
+                      height='50'
+                      fill='white'
+                      viewBox='0 0 50 50'
+                    >
+                      <path d='M 25 1.0507812 C 24.7825 1.0507812 24.565859 1.1197656 24.380859 1.2597656 L 1.3808594 19.210938 C 0.95085938 19.550938 0.8709375 20.179141 1.2109375 20.619141 C 1.5509375 21.049141 2.1791406 21.129062 2.6191406 20.789062 L 4 19.710938 L 4 46 C 4 46.55 4.45 47 5 47 L 19 47 L 19 29 L 31 29 L 31 47 L 45 47 C 45.55 47 46 46.55 46 46 L 46 19.710938 L 47.380859 20.789062 C 47.570859 20.929063 47.78 21 48 21 C 48.3 21 48.589063 20.869141 48.789062 20.619141 C 49.129063 20.179141 49.049141 19.550938 48.619141 19.210938 L 25.619141 1.2597656 C 25.434141 1.1197656 25.2175 1.0507812 25 1.0507812 z M 35 5 L 35 6.0507812 L 41 10.730469 L 41 5 L 35 5 z'></path>
+                    </svg>
+                  </div>
+                </Link>
+                <Link to='/lobby' className={styles.buttonMenu}>
+                  Back to lobby
                   <img src='video/player.gif' className={styles.multiplayerGif1Menu} />
                   <img src='video/player.gif' className={styles.multiplayerGif2Menu} />
-                  {/* <img src='video/player.gif' className={styles.multiplayerGif3Menu} /> */}
-                </button>
-              </Link>
-              <div className={styles.myComponent}>
-                <div className={styles.playerInfo}>
-                  {/* <div className={styles.playerName}>{auth.name}</div> */}
-                  <div className={styles.playerStats}>
-                    Points: 0<br />
-                  </div>
-                </div>
+                </Link>
               </div>
+              {/* <div className={styles.myComponent}>
+                <div className={styles.playerAvatar}>
+                  <img
+                    src='images/avatar.png'
+                    alt='Player Avatar'
+                    className={styles.playerAvatarImg}
+                  />
+                </div>
+                <div className={styles.playerInfo}>
+                  <div className={styles.playerName}>{auth.name}</div>
+                </div>
+              </div> */}
             </div>
           </div>
         </div>
