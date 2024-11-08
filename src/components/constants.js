3fee36d0fb00d4afd34b82f7684887b564382909
GradinaruAlexandra <147167654+GradinaruAlexandra@users.noreply.github.com>
2024-11-08 20:00:23 +0200

    .

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,8 +1,8 @@
-// export const API_SELF = 'http://192.168.1.174:8000';
+export const API_SELF = 'http://192.168.1.174:8000';
 
-export const API_SELF = 'https://lrsback-lrs-bd4d9a06.koyeb.app';
-export const WS_URL = 'wss://lrsback-lrs-bd4d9a06.koyeb.app';
-// export const WS_URL = 'ws://192.168.1.174:8000';
+// export const API_SELF = 'https://lrsback-lrs-bd4d9a06.koyeb.app';
+// export const WS_URL = 'wss://lrsback-lrs-bd4d9a06.koyeb.app';
+export const WS_URL = 'ws://192.168.1.174:8000';
 
 export const AUTHENTICATE = '/auth/register';
 export const LOGIN = '/auth/token';
